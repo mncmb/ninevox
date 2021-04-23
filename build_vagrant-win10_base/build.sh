29759da41cb77c6827153dc960a55598e4def4dd
mncmbb <mncmbb>
2021-04-23 17:15:21 +0200

    comments & structure for build script and box dl

--- build_vagrant-win10_base/build.sh
+++ build_vagrant-win10_base/build.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+#################################################################
+#
+#   Download Vagrant Win10Edge VM image (90 day trial) and 
+#   add the box to Vagrant
+#
+#################################################################
+
 FILENAME="vagrantwin10.zip"
 VAGRANTIMG="windows/win10-edge"
 UNZIPPED="MSEdge - Win10.box"
+
+# download the box image file
 python3 downloader.py $FILENAME
+
 if [ $? -eq 0 ];
 then
     # download successful
@@ -12,5 +22,5 @@ then
     echo "INFO:Image extracted. Adding box to Vagrant"
     vagrant box add $VAGRANTIMG $UNZIPPED
 else
-    echo else case
+    echo "Download failed. Wrong hashes"
 fi
