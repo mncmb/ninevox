912ebd1899604568502b575ac646e84ffd5c9221
mncmbb <mncmbb>
2021-05-12 21:30:03 +0200

    switched to vagrant

---
+++ Win10Dev/README.md
@@ -0,0 +1,8 @@
+# init
+Check if winrm listener is running
+
+```
+winrm e winrm/config/listener
+```
+
+Check for colliding ports / in use
--- Win10Dev/bootstrap.ps1
+++ Win10Dev/bootstrap.ps1
@@ -15,24 +15,24 @@ cmd /C "winrm quickconfig -q"
 
 # activate openSSH
 # use the script deployed by microsoft 
-echo "[INFO] setting up SSH server:"
-C:\BGinfo\openssh.ps1
+# echo "[INFO] setting up SSH server:"
+# C:\BGinfo\openssh.ps1
 
 # set background to something not completely boring
-mv C:\vagrant\files\modern.IE.1024x768.jpg C:\BGinfo\modern.IE.1024x768.jpg
-
+#cp C:\vagrant\files\BGCONFIG.BGI C:\BGinfo\BGCONFIG.BGI
+#cp C:\vagrant\files\modern.IE.1024x768.jpg C:\BGinfo\modern.IE.1024x768.jpg
 
 # install chocolatey
-echo "[INFO] installing chocolatey package manager:" 
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+# echo "[INFO] installing chocolatey package manager:" 
+# Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # install packages and create symlink to installs
-echo "[INFO] run chocolatey installations:"
-C:\vagrant\choco-install.ps1
+# echo "[INFO] run chocolatey installations:"
+# C:\vagrant\choco-install.ps1
 
 # create desktop links to programs
 # cmd /C mklink /d C:\Users\IEUser\Desktop\tools C:\ProgramData\chocolatey\bin
-Set-ExecutionPolicy Bypass -Scope Process -Force; C:\vagrant\create_choco_shortcut.ps1 x32dbg x64dbg pestudio pe-bear
+# Set-ExecutionPolicy Bypass -Scope Process -Force; C:\vagrant\create_choco_shortcut.ps1 x32dbg x64dbg pestudio pe-bear
 
 # TODO: install and use puppet
 # download from https://downloads.puppetlabs.com/windows/puppet5/
